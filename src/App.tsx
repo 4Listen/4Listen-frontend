506a73a521f383f4a129dc0bb8e30313f3694363
André Ramos <andre_ramosdovale@outlook.com>
2022-08-08 10:53:44 -0300

    [refactor] - remove codesmell

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,5 @@
 import { FC } from 'react'
-import { BrowserRouter } from 'react-router-dom'
-import { Route, Routes } from 'react-router-dom'
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import Home from './pages/Home/Home'
 
 const App: FC = () => {
