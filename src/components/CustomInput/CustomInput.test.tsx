574fe1f31a67141123b3cad117d32c51da0e537b
André Ramos <andre_ramosdovale@outlook.com>
2022-08-08 15:25:19 -0300

    [feature] - create inputs components and mask for cep, cpf and currency

--- src/components/CustomInput/CustomInput.test.tsx
+++ src/components/CustomInput/CustomInput.test.tsx
@@ -1,18 +1,35 @@
 import { render, screen } from '@testing-library/react'
-import { CustomDefaultInput, CustomDefaultInputWithTitle } from './CustomInput'
+import {
+  CustomCEPInput,
+  CustomCPFInput,
+  CustomCurrencyInput,
+  CustomDefaultInput,
+} from './CustomInput'
 import { defaultProps } from './CustomInput.data'
 
 describe('<CustomInput /> Unit', () => {
-  it('Should render default input correctly', () => {
-    render(<CustomDefaultInput {...defaultProps} />)
-    expect(screen.getByPlaceholderText(defaultProps.placeholder)).toBeTruthy()
-    expect(screen.getByDisplayValue(defaultProps.value)).toBeTruthy()
-  })
-
   it('Should render default input with title correctly', () => {
-    render(<CustomDefaultInputWithTitle {...defaultProps} />)
+    render(<CustomDefaultInput {...defaultProps} />)
+
     expect(screen.getByText(defaultProps.title)).toBeTruthy()
     expect(screen.getByPlaceholderText(defaultProps.placeholder)).toBeTruthy()
-    expect(screen.getByDisplayValue(defaultProps.value)).toBeTruthy()
+  })
+
+  it('Should render cpf input with correctly', () => {
+    render(<CustomCEPInput {...defaultProps} />)
+
+    expect(screen.getByText(defaultProps.title)).toBeTruthy()
+  })
+
+  it('Should render cpf input with correctly', () => {
+    render(<CustomCPFInput {...defaultProps} />)
+
+    expect(screen.getByText(defaultProps.title)).toBeTruthy()
+  })
+
+  it('Should render cpf input with correctly', () => {
+    render(<CustomCurrencyInput {...defaultProps} />)
+
+    expect(screen.getByText(defaultProps.title)).toBeTruthy()
   })
 })
--- src/components/CustomInput/CustomInput.tsx
+++ src/components/CustomInput/CustomInput.tsx
@@ -1,21 +1,51 @@
-import { FC, InputHTMLAttributes } from 'react'
+import { FC, InputHTMLAttributes, useCallback } from 'react'
 import { FormControl, FormLabel, Input } from '@chakra-ui/react'
+import { cep, cpf, currency } from '../../utils/masks/masks'
 
 export const CustomDefaultInput: FC<InputHTMLAttributes<HTMLInputElement>> = ({ ...props }) => {
   return (
     <FormControl>
-      <Input variant="outline" placeholder={props.placeholder} value={props.value} />
+      <FormLabel>{props.title}</FormLabel>
+      <Input variant="outline" placeholder={props.placeholder} />
     </FormControl>
   )
 }
 
-export const CustomDefaultInputWithTitle: FC<InputHTMLAttributes<HTMLInputElement>> = ({
-  ...props
-}) => {
+export const CustomCPFInput: FC<InputHTMLAttributes<HTMLInputElement>> = ({ ...props }) => {
+  const handleKeyUp = useCallback((e: React.FormEvent<HTMLInputElement>) => {
+    cpf(e)
+  }, [])
+
   return (
     <FormControl>
       <FormLabel>{props.title}</FormLabel>
-      <Input variant="outline" placeholder={props.placeholder} value={props.value} />
+      <Input variant="outline" placeholder="999.999.999-99" onKeyUp={handleKeyUp} />
+    </FormControl>
+  )
+}
+
+export const CustomCEPInput: FC<InputHTMLAttributes<HTMLInputElement>> = ({ ...props }) => {
+  const handleKeyUp = useCallback((e: React.FormEvent<HTMLInputElement>) => {
+    cep(e)
+  }, [])
+
+  return (
+    <FormControl>
+      <FormLabel>{props.title}</FormLabel>
+      <Input variant="outline" placeholder="99.999.999" onKeyUp={handleKeyUp} />
+    </FormControl>
+  )
+}
+
+export const CustomCurrencyInput: FC<InputHTMLAttributes<HTMLInputElement>> = ({ ...props }) => {
+  const handleKeyUp = useCallback((e: React.FormEvent<HTMLInputElement>) => {
+    currency(e)
+  }, [])
+
+  return (
+    <FormControl>
+      <FormLabel>{props.title}</FormLabel>
+      <Input variant="outline" prefix="R$" placeholder="0,00" onKeyUp={handleKeyUp} />
     </FormControl>
   )
 }
